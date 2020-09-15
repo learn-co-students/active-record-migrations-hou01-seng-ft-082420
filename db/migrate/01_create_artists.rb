6a6b2151d7870da74814c7f4d7843daf77726cee
MSarfarazH <m.s.hussain@outlook.com>
2020-09-15 18:43:12 -0500

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -9,19 +9,3 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     end
 end
 
-ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/artists.sqlite"
-)
-
-sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS artists(
-        id INTERGER PRIMARY KEY,
-        name TEXT,
-        genre TEXT,
-        age INTEGER,
-        hometown TEXT
-    )
-SQL
-
-ActiveRecord::Base.connection.execute(sql)
